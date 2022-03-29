f1fc6b1267036a41b8bd368e288a72f725da01ca
jolkarnine <julkernainm@gmail.com>
2022-03-29 20:13:22 +0600

    first

---
+++ script.js
@@ -0,0 +1,20 @@
+var prothom = document.querySelector(".ad")
+
+var ditiyo = document.querySelector(".btn")
+
+// console.log(prothom)
+// console.log(ditiyo)
+
+ditiyo.addEventListener("click",function(){
+    // console.log(prothom.value)
+
+    for (i=1;i<=10;i++){
+        document.write(prothom.value + " x " + i + " = " + prothom.value*i + "<br>")
+        document.body.style.background='#000'
+        document.body.style.color='#fff'
+        document.body.style.textAlign="center"
+        document.body.style.marginTop='250px'
+        document.body.style.fontFamily='Oswald, sans-serif'
+        document.body.style.fontSize='20px'
+    }
+})
